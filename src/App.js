fb243f30f76c7073f29ea828f85bd3abfe2564f4
damara <damarmuhamad13@gmail.com>
2024-05-02 20:53:20 +0700

    feat: update initialfriend in initialfriends.jsx

--- src/App.js
+++ src/App.js
@@ -2,26 +2,7 @@ import React from 'react'
 import FriendList from './components/FriendList';
 import FormAddFriend from './components/FormAddFriend';
 import FormSplitBills from './components/FormSplitBills';
-const InitialFriends = [
-  {
-    id: 118836,
-    name: "Diamond Star",
-    image: "https://i.pravatar.cc/48?u=118836",
-    balance: -7,
-  },
-  {
-    id: 933372,
-    name: "Alton Zone",
-    image: "https://i.pravatar.cc/48?u=933372",
-    balance: 20,
-  },
-  {
-    id: 499476,
-    name: "Alice Smith",
-    image: "https://i.pravatar.cc/48?u=499476",
-    balance: 0,
-  },
-];
+import InitialFriends from './components/InitialFriends';
 
 const App = () => {
   return (
