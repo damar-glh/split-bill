19bf0678688967eb611c0bc7f2105b769e044463
damara <damarmuhamad13@gmail.com>
2024-05-11 15:03:10 +0700

    feat: Showing and hiding the Add Friends form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import FriendList from './components/FriendList';
 import FormAddFriend from './components/FormAddFriend';
 import FormSplitBills from './components/FormSplitBills';
 import InitialFriends from './components/InitialFriends';
 
 const App = () => {
+  const [showAddFriend, setShowAddFriend] = useState(false);
+  function handleShowAddFriend(){
+    setShowAddFriend((showAddFriend) => !showAddFriend);
+  }
   return (
     <div className="app">
       <div className="sidebar">
         <FriendList friends={InitialFriends} />
-        <FormAddFriend />
-        <button className='button'>Add Friend's</button>
+        {showAddFriend && <FormAddFriend/>}
+        <button className='button' onClick={handleShowAddFriend}>{showAddFriend ? "Close Friend's" : "Add Friend's"}</button>
       </div>
       <FormSplitBills />
     </div>
